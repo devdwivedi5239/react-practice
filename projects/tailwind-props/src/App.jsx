71e45fea484319ede0e13739c80a715a959bbfba
Dev Dwivedi <devsdwivedi@gmail.com>
2024-01-10 00:15:14 +0530

    tailwindAndProps

---
+++ projects/tailwind-props/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './card'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tailwind and props</h1>
+      <Card username="Devika" btnText="View Profile"/>
+      <Card username="Binodni"/>
+    </>
+  )
+}
+
+export default App
