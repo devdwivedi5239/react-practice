99fb538c5be65f7406012bad5da45d6beb2e2d4f
Dev Dwivedi <devsdwivedi@gmail.com>
2024-01-15 23:24:32 +0530

    q-2

---
+++ practice-questions/src/DynamicContent.jsx
@@ -0,0 +1,8 @@
+
+function DynamicContent({prop}) {
+  return (
+    <div>DynamicContent component is currently used for displaying {prop}</div>
+  )
+}
+
+export default DynamicContent
--- practice-questions/src/main.jsx
+++ practice-questions/src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Greetings from "./Greetings.jsx";
+import DynamicContent from "./DynamicContent.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* JSX Rendering:
-Create a functional component called Greetings that renders a greeting message with your name using JSX.
-Render this component in the root of your React application. */}
-    <Greetings />
+    Create a functional component called Greetings that renders a greeting message with your name using JSX.
+    Render this component in the root of your React application. */}
+    {/* <Greetings /> */}
 
-    
+    {/* Dynamic Content:
+    Create a functional component called DynamicContent that takes a prop content and displays it in a paragraph using JSX. */}
+    <DynamicContent prop="I dont know how this works"/>
   </React.StrictMode>
 );
