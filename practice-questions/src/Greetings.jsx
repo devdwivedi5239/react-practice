7c8f790d06e81251040cf20be7c09be2ec9c9395
Dev Dwivedi <devsdwivedi@gmail.com>
2024-01-15 23:12:11 +0530

    q-1

---
+++ practice-questions/src/Greetings.jsx
@@ -0,0 +1,12 @@
+// JSX Rendering:
+// Create a functional component called Greetings that renders a greeting message with your name using JSX.
+// Render this component in the root of your React application.
+
+
+function greetings() {
+  return (
+    <div>greetings Dev</div>
+  )
+}
+
+export default greetings
--- practice-questions/src/main.jsx
+++ practice-questions/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Greetings from "./Greetings.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* JSX Rendering:
+Create a functional component called Greetings that renders a greeting message with your name using JSX.
+Render this component in the root of your React application. */}
+    <Greetings />
+
+    
+  </React.StrictMode>
+);
